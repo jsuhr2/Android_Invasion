714eea85443e00d5418b06142d658ae0b0527e1c
Jasper Suhr <jsuhr2@bl-n17.pods.bu.int>
2019-02-27 01:17:15 -0500

    have enemies and all movement and firing working, need to work on collisions, lives, and score

--- CS441Project3/CS441Project3/GameScene.swift
+++ CS441Project3/CS441Project3/GameScene.swift
@@ -9,11 +9,26 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
     
     let madden = SKSpriteNode(imageNamed: "Madden_Glasses")
     
     let gameArea: CGRect
+    
+    func random() -> CGFloat{
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    func random(min:CGFloat, max: CGFloat) -> CGFloat{
+        return random() * (max - min) + min
+    }
+    
+    struct PhysicsCategories{
+        static let None : UInt32 = 0
+        static let Madden : UInt32 = 0b1 //1
+        static let Blast : UInt32 = 0b10 //2
+        static let Enemy : UInt32 = 0b100 //3
+    }
+    
     override init(size: CGSize){
         let maxAspectRatio: CGFloat = 16.0/9.0
         let playableWidth = size.height/maxAspectRatio
@@ -27,6 +42,8 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
+        
+        self.physicsWorld.contactDelegate = self
 
         let background = SKSpriteNode(imageNamed: "space")
         background.size = self.size
@@ -36,9 +53,58 @@ class GameScene: SKScene {
  
         backgroundColor = SKColor.white
         madden.setScale(2.5)
-        madden.position = CGPoint(x: size.width/2, y: size.height/5)
+        madden.position = CGPoint(x: self.size.width/2, y: self.size.height/5)
         madden.zPosition = 2
-        addChild(madden)
+        madden.physicsBody = SKPhysicsBody(rectangleOf: madden.size)
+        madden.physicsBody!.affectedByGravity = false
+        madden.physicsBody!.categoryBitMask = PhysicsCategories.Madden
+        madden.physicsBody!.collisionBitMask = PhysicsCategories.None
+        madden.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
+        self.addChild(madden)
+        
+        start()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+    }
+    
+    func start(){
+        let spawn = SKAction.run(spawnEnemy)
+        let wait = SKAction.wait(forDuration: 1)
+        let spawnSequence = SKAction.sequence([wait, spawn])
+        let spawnContinuous = SKAction.repeatForever(spawnSequence)
+        self.run(spawnContinuous)
+    }
+    
+    func spawnEnemy(){
+        let startX = random(min: gameArea.minX, max: gameArea.maxX)
+        let endX = random(min: gameArea.minX, max: gameArea.maxX)
+        
+        let start = CGPoint(x: startX, y: self.size.height * 1.1)
+        let end = CGPoint(x: endX, y: -self.size.height * 0.1)
+        
+        let enemy = SKSpriteNode(imageNamed: "Android_Studio_icon")
+        enemy.setScale(0.1)
+        enemy.position = start
+        enemy.zPosition = 2
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody!.affectedByGravity = false
+        enemy.physicsBody!.categoryBitMask = PhysicsCategories.Enemy
+        enemy.physicsBody!.collisionBitMask = PhysicsCategories.None
+        enemy.physicsBody!.contactTestBitMask = PhysicsCategories.Madden | PhysicsCategories.Blast
+        self.addChild(enemy)
+        
+        let dx = end.x - start.x
+        let dy = end.y - start.y
+        let rotate = atan2(dy, dx) + 3.141659/2
+        enemy.zRotation = rotate
+        
+        let moveEnemy = SKAction.move(to: end, duration: 4)
+        let deleteEnemy = SKAction.removeFromParent()
+        let enemySequence = SKAction.sequence([moveEnemy, deleteEnemy])
+        enemy.run(enemySequence)
+        
     }
     
     func fire(){
@@ -46,6 +112,11 @@ class GameScene: SKScene {
         blast.setScale(0.023)
         blast.position = madden.position
         blast.zPosition = 1
+        blast.physicsBody = SKPhysicsBody(rectangleOf: blast.size)
+        blast.physicsBody!.affectedByGravity = false
+        blast.physicsBody!.categoryBitMask = PhysicsCategories.Blast
+        blast.physicsBody!.collisionBitMask = PhysicsCategories.None
+        blast.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(blast)
         
         let moveBlast = SKAction.moveTo(y: self.size.height+blast.size.height, duration: 1)
@@ -62,14 +133,24 @@ class GameScene: SKScene {
         for touch: AnyObject in touches{
             let pointOfTouch = touch.location(in: self)
             let previousPointOfTouch = touch.previousLocation(in: self)
-            let amountDragged = pointOfTouch.x - previousPointOfTouch.x
-            madden.position.x += amountDragged
-            
+            let horizontalDragged = pointOfTouch.x - previousPointOfTouch.x
+            madden.position.x += horizontalDragged
+
+            let verticalDragged = pointOfTouch.y - previousPointOfTouch.y
+            madden.position.y += verticalDragged
+
             if madden.position.x > gameArea.maxX - madden.size.width/2{
                 madden.position.x = gameArea.maxX - madden.size.width/2
             }
             if madden.position.x < gameArea.minX + madden.size.width/2{
                 madden.position.x = gameArea.minX + madden.size.width/2
+            }
+
+            if madden.position.y > gameArea.maxY - madden.size.height/2{
+                madden.position.y = gameArea.maxY - madden.size.height/2
+            }
+            if madden.position.y < gameArea.minY + madden.size.height/2{
+                madden.position.y = gameArea.minY + madden.size.height/2
             }
         }
     }
